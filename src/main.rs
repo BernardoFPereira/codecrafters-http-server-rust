8f6efcfe6c7c95a354f683e3dbceb141fa7bbf13
Beetledjuice <bernardofpereira@live.com>
2024-06-25 14:18:23 -0300

    test ok response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::io::Write;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,12 +11,22 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                connection_ok(&mut stream);
+                crlf(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn connection_ok(stream: &mut TcpStream) -> usize {
+    stream.write(b"HTTP/1.1 200 OK\r\n").unwrap()
+}
+
+fn crlf(stream: &mut TcpStream) -> usize {
+    stream.write(b"\r\n").unwrap()
 }
